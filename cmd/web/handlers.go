0ac2fb12c674db82c5ae0a4d9930791b6ddee1cb
Emil Nuutinen <emil@emilnuutinen.com>
2023-04-08 10:06:00 +0300

    Chapter 5 finished

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	// "html/template"
 
 	"github.com/emilnuutinen/snippetbox/internal/models"
 )
@@ -22,28 +21,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
-	}
-	//
-	//	files := []string{
-	//		"./ui/html/base.tmpl",
-	//		"./ui/html/partials/nav.tmpl",
-	//		"./ui/html/pages/home.tmpl",
-	//	}
-	//
-	//	ts, err := template.ParseFiles(files...)
-	//	if err != nil {
-	//		app.serverError(w, err)
-	//		return
-	//	}
-	//
-	//	err = ts.ExecuteTemplate(w, "base", nil)
-	//	if err != nil {
-	//		app.serverError(w, err)
-	//		return
-	//	}
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
 
+	app.render(w, http.StatusOK, "home.tmpl", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -62,8 +43,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 
-	fmt.Fprintf(w, "%+v", snippet)
+	app.render(w, http.StatusOK, "view.tmpl", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
