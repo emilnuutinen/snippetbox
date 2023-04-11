5b79a4329755d241d19ff4cfaaebbe300b10c0f7
Emil Nuutinen <emil@emilnuutinen.com>
2023-04-11 09:36:18 +0300

    Chapter 9 finished

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -44,6 +44,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
 
@@ -93,6 +94,8 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.serverError(w, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Snippet succesfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 
