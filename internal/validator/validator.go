26b14522249aaa249644e8ec18de41778629d07c
Emil Nuutinen <emil@emilnuutinen.com>
2023-04-12 10:40:51 +0300

    Chapter 11 finished

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,16 +1,27 @@
 package validator
 
 import (
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
 
+// https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
 // Valid() returns true if the FieldErrors map doesn't contain any entries.
-func (v *Validator) Valid() bool { return len(v.FieldErrors) == 0 }
+func (v *Validator) Valid() bool {
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
+}
 
 // AddFieldError() adds an error message to the FieldErrors map (so long as no
 // entry already exists for the given key).
@@ -52,4 +63,12 @@ func PermittedInt(value int, permittedValues ...int) bool {
 		}
 	}
 	return false
+}
+
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
 }
